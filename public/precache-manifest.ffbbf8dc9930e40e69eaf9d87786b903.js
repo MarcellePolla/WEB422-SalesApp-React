3b7fedb59d0c0a607ac466a679166979cc61ba28
Marcelle <cellypolla@gmail.com>
2020-09-02 15:23:37 -0400

    First commit

---
+++ public/precache-manifest.ffbbf8dc9930e40e69eaf9d87786b903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde6c7903a8147fe72ad4645e5f57d67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d62db9b82ecf274bf8d",
+    "url": "/static/css/main.3e6bca19.chunk.css"
+  },
+  {
+    "revision": "29e674d88f7ffa1760a8",
+    "url": "/static/js/2.d817c68f.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/static/js/2.d817c68f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d62db9b82ecf274bf8d",
+    "url": "/static/js/main.6b893c6e.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
